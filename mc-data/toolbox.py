b8a59044b2d346334309adb3475e15db9376fc31
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2016-05-11 16:38:40 +0100

    alpha_S dictionary

--- mc-data/toolbox.py
+++ mc-data/toolbox.py
@@ -4,12 +4,12 @@ import math
 # Conversion of LHAPDF ID to alpha_S value
 def lha_to_alphas(lhaid):
 	pdfsets={
-		264000 : 0.115,
-		265000 : 0.117,
-		266000 : 0.119,
-		267000 : 0.121,
-		260000 : 0.118 
+		264 : 0.115,
+		265 : 0.117,
+		266 : 0.119,
+		267 : 0.121,
+		260 : 0.118 
 	}
 
-	baseset = 1000*math.floor(lhaid/1000.0)
+	baseset = int(math.floor(lhaid/1000.0))
 	return pdfsets[baseset]
