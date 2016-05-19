b8f6c72dd4030f270dc545fdf485eec37c9f39ea
Enrico Bothmann <enrico.bothmann@phys.uni-goettingen.de>
2016-05-19 13:11:07 +0200

    Add MENLOPS production setup

---
+++ sherpa-production-setup/MENLOPS/resources.py
@@ -0,0 +1,10 @@
+"""
+Definition of the resources used for runs in this directory.
+Walltime in minutes, mem[ory] in GB.
+"""
+resources = {}
+resources['single']      = {'walltime': 60,     'mem': 2}
+resources['integration'] = {'walltime': 2*60,     'mem': 2, 'nodes': 64}
+# resources['integration'] = {'walltime': 60,     'mem': 2}
+resources['reweighting'] = {'walltime': 60,     'mem': 2}
+
