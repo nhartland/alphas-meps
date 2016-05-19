f049c5ad0efa30f0a9e68c6a338ddf007033c216
Enrico Bothmann <enrico.bothmann@phys.uni-goettingen.de>
2016-05-19 13:13:44 +0200

    Adjust NLOPS production setup

--- sherpa-production-setup/NLOPS/resources.py
+++ sherpa-production-setup/NLOPS/resources.py
@@ -3,8 +3,8 @@ Definition of the resources used for runs in this directory.
 Walltime in minutes, mem[ory] in GB.
 """
 resources = {}
-resources['single']      = {'walltime': 2,      'mem': 2}
-# resources['integration'] = {'walltime': 60,     'mem': 2, 'nodes': 64}
-resources['integration'] = {'walltime': 60,     'mem': 2}
-resources['reweighting'] = {'walltime': 45,     'mem': 8}
+resources['single']      = {'walltime': 60,   'mem': 2}
+# resources['integration'] = {'walltime': 60,   'mem': 2, 'nodes': 64}
+resources['integration'] = {'walltime': 60,   'mem': 2}
+resources['reweighting'] = {'walltime': 60,   'mem': 2}
 
